2a089f373d9d7cde33d82e002425f1e531065265
Jean Folle Vanz <jeanfolle86@gmail.com>
2024-09-03 22:49:31 -0300

    Ajustes na Estilização da Página de Criadores
    Também foram removidos os arquivos extras de fontes externas, já que não estavam sendo utilizados.

--- src/pages/Creators.js
+++ src/pages/Creators.js
@@ -4,20 +4,25 @@ import React from "react";
 export default function Creators() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Jean Folle Vanz</Text>
-            <Text style={styles.text}>RA: 1134254</Text>
-            <Text style={styles.text}>Email: 1134254@atitus.edu.br</Text>
-            <Text></Text>
-            <Text></Text>
-            <Text></Text>
-            <Text></Text>
-            <Text style={styles.text}>Maria Eduarda Carvalho Dornelles</Text>
-            <Text style={styles.text}>RA: 1134791</Text>
-            <Text style={styles.text}>Email: 1134791@atitus.edu.br</Text>
+            <View style={styles.container1}>
+                <Text style={styles.text}>Jean Folle Vanz</Text>
+                <Text style={styles.text}>RA: 1134254</Text>
+                <Text style={styles.text}>Email: 1134254@atitus.edu.br</Text>
+            </View>
+            <View style={styles.container1}>
+                <Text style={styles.text}>Maria Eduarda Carvalho Dornelles</Text>
+                <Text style={styles.text}>RA: 1134791</Text>
+                <Text style={styles.text}>Email: 1134791@atitus.edu.br</Text>
+            </View>
         </View>
     );
 }
 const styles = StyleSheet.create({
+    container1: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 50
+    },
     container: {
         flex: 1,
         justifyContent: "center",
